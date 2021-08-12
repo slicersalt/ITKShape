d8871645c3f4ae318238cd6c01ee9cc4f20ac4e3
Tom Birdsong <tom.birdsong@kitware.com>
2021-08-12 13:03:07 -0400

    ENH: Update project name in CMake and fix wrappings

--- itk-module.cmake
+++ itk-module.cmake
@@ -3,15 +3,15 @@
 get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 file(READ "${MY_CURRENT_DIR}/README.rst" DOCUMENTATION)
 
-# itk_module() defines the module dependencies in Mesh3DProcrustesAlignFilter
-# Mesh3DProcrustesAlignFilter depends on ITKCommon
-# The testing module in Mesh3DProcrustesAlignFilter depends on ITKTestKernel
-# and ITKMetaIO(besides Mesh3DProcrustesAlignFilter and ITKCore)
+# itk_module() defines the module dependencies in ITKShape
+# ITKShape depends on ITKCommon
+# The testing module in ITKShape depends on ITKTestKernel
+# and ITKMetaIO(besides ITKShape and ITKCore)
 # By convention those modules outside of ITK are not prefixed with
 # ITK.
 
 # define the dependencies of the include module and the tests
-itk_module(Mesh3DProcrustesAlignFilter
+itk_module(Shape
   DEPENDS
     ITKCommon
     ITKMesh
