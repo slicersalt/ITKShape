0c8b94e4f1fc493828efd1f1118272a2479b067f
Matt McCormick <matt.mccormick@kitware.com>
2022-12-16 17:22:07 -0500

    ENH: Bump CI and Python package for ITK 5.3.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='itk-shape',
-    version='0.2.2',
+    version='0.3.0',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
@@ -43,6 +43,6 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://itk.org/',
     install_requires=[
-        r'itk>=5.3rc04.post3'
+        r'itk>=5.3.0'
     ]
     )
